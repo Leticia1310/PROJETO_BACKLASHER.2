412c203a5cd92a1df26348ead2d53e1677011e0d
Leticia G. <leticiaguedes476@gmail.com>
2026-06-16 15:17:30 -0300

    Update DisponibilidadeController.java

--- projeto-backend-v1-main/projeto-backend-v1-main/src/main/java/br/com/backend/backlashes_api/controller/DisponibilidadeController.java
+++ projeto-backend-v1-main/projeto-backend-v1-main/src/main/java/br/com/backend/backlashes_api/controller/DisponibilidadeController.java
@@ -20,10 +20,35 @@ public class DisponibilidadeController {
     public List<Disponibilidade> listar() {
         return repository.findAll();
     }
-
+    @GetMapping("/{id}")
+    public ResponseEntity<Disponibilidade> buscar(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(disponibilidade -> ResponseEntity.ok(disponibilidade))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
      @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Disponibilidade criar(@RequestBody Disponibilidade disponibilidade) {
         return repository.save(disponibilidade);
+    }
+    @PutMapping("/{id}")
+public ResponseEntity<Disponibilidade> atualizar(@PathVariable Long id, @RequestBody Disponibilidade disponibilidadeAtualizada) {
+    return repository.findById(id)
+            .map(disponibilidade -> {
+                disponibilidade.setDiasemana(disponibilidadeAtualizada.getDiasemana());
+                disponibilidade.setHorainicio(disponibilidadeAtualizada.getHorainicio());
+                disponibilidade.setHorafim(disponibilidadeAtualizada.getHorafim());
+                
+                disponibilidade.setProfissional(disponibilidadeAtualizada.getProfissional());
+                return ResponseEntity.ok(repository.save(disponibilidade));
+            })
+            .orElse(ResponseEntity.notFound().build());
+    
+        }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
